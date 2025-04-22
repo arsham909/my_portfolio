acd0128c7f2a49330579cb28bf51e8183d1e9550
arsham <arsham202@gmail.com>
2025-04-22 17:13:09 +0400

    view admin added

--- portfolio/blog/admin.py
+++ portfolio/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish']
+    list_filter = ['title', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug':('title',)}
