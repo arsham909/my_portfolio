0767b07c272c214c227e0ed11dd193603ab5c560
arsham <arsham202@gmail.com>
2025-04-22 14:17:29 +0400

    added blog models to it

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    slug = models.SlugField(max_length=250)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_created=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_post'
+    )
+    
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+    
+    
+    def __str__(self):
+        return self.title
