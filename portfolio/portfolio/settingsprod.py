344b0452032f949d6b0269d6a609e0e83af5be6c
arsham <arsham202@gmail.com>
2025-04-24 18:33:47 +0400

    change the database to sqlite

--- portfolio/portfolio/settingsprod.py
+++ portfolio/portfolio/settingsprod.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'portfolio',
-        'USER': 'portfoliouser',
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST': 'localhost',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
