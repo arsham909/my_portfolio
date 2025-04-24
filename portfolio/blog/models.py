3981598bd43154c3022b8a12e3a828b0f932575d
arsham <arsham202@gmail.com>
2025-04-24 12:40:46 +0400

    added image to sve in blog and latest blog and project will be display on homepage

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -14,8 +14,6 @@ class Post(models.Model):
         DRAFT = 'Df', 'Draft'
         PUBLISHED = 'PB' , 'Published'
         
-    
-    
     title =  models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     author = models.ForeignKey(
@@ -27,6 +25,7 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    image = models.ImageField(null=True, blank=True, upload_to="images/")
     
     status = models.CharField(
         max_length=2,
--- portfolio/homepage/views.py
+++ portfolio/homepage/views.py
@@ -5,5 +5,6 @@ from blog.views import PostListView
 # Create your views here.
 def homepage(request):
     recentPosts = Post.published.order_by('-publish')[:3]
+    projects = Post.published.filter(tags__in=[8])
     
-    return render(request, "homepage/homepage.html",{'recentPosts':recentPosts})
+    return render(request, "homepage/homepage.html",{'recentPosts':recentPosts, 'projects':projects} )
