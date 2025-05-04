d595948177979614ee6d979e2e09ed98cab88983
arsham <arsham202@gmail.com>
2025-05-04 10:37:22 +0400

    setup the nginx and uwsgi

--- portfolio/portfolio/settings/prod.py
+++ portfolio/portfolio/settings/prod.py
@@ -1,4 +1,5 @@
 from .base import *
+from decouple import config
 
 DEBUG = False
 
@@ -10,7 +11,11 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':  'django.db.backends.postgresql',
+        'NAME':  config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
